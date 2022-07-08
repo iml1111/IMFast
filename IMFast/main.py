e3a54598402d4c09d9521107c6d914249faf2449
IML <shin10256@gmail.com>
2022-07-08 22:10:31 +0900

    App Routes Command 구현
    Update main.py

--- IMFast/main.py
+++ IMFast/main.py
@@ -2,6 +2,7 @@
 Application Management Module
 """
 import click
+from fastapi.routing import APIRoute
 from app import create_app
 from settings import settings
 
@@ -28,8 +29,34 @@ def prod_run():
 @cli.command()
 def init_db():
     """Sample command"""
-    click.echo('Initialized the database')
+    click.echo('Initialized the database (if necessary)')
 
+
+@cli.command()
+def routes():
+    """Print all routes"""
+    click.echo('# Routes')
+    routes = []
+    path_len = 0
+    method_len = 0
+    name_len = 0
+
+    for route in application.routes:
+        routes.append((
+            route.path, str(route.methods), route.name))
+        path_len = max(path_len, len(route.path))
+        method_len = max(method_len, len(str(route.methods)))
+        name_len = max(name_len, len(route.name))
+
+    click.echo(
+        f"{'Path ':=<{path_len + 2}}"
+        f"{' Methods ':=<{method_len + 1}}"
+        f"{' Name ':=<{name_len + 1}}")
+    for route in sorted(routes):
+        click.echo(
+            f'{route[0]: <{path_len + 2}}'
+            f'{route[1]: <{method_len + 2}}'
+            f'{route[2]: <{name_len + 2}}')
 
 if __name__ == '__main__':
     cli()
